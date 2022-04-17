ac19930f47db1ef7fb8851e3515f02e204bd1688
Akash Achuzzz <99610857+AkashAchuzzz@users.noreply.github.com>
2022-04-17 17:07:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -404,14 +404,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true'),
-            InlineKeyboardButton('📖 Instructions', url='https://telegra.ph/THOR-BOT---Instructions-04-17')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Support', url='https://t.me/ThorBotSupport')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('😊 About', callback_data='about'),
+            InlineKeyboardButton('📖 Instructions', url='https://telegra.ph/THOR-BOT---Instructions-04-17')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
